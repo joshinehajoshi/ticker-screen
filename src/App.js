1581b2630a7fc1cdc34319b04736d6176e5f927b
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-14 16:22:51 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 
 function App() {
   
-  const ws = new WebSocket('wss://localhost:3000/stg.clubdefy.io/to/ws');
+  // const ws = new WebSocket('wss://localhost:3000/stg.clubdefy.io/to/ws');
 
-  ws.onmessage = (event) => {
-    console.log(event.data)
-  }
+  // ws.onmessage = (event) => {
+  //   console.log(event.data)
+  // }
+
   return (
-    <div className="App">
-      
+    <div className="App row p-0 m-0">
+      <div className="App-div1">
+        <p className="App-p1">Hi Jhon,</p>
+        <p className="App-p2">Complete your KYC</p>
+        <p className="App-p3">and experience the world class <br /> bitcoin app defi</p>
+        <p className="App-p4">Start KYC <i class="fa-solid fa-arrow-right-long"></i></p>
+      </div>
+      <div className="App-div2">
+        <h1>1</h1>
+      </div>
+      <div className="App-div3">
+        <h1>2</h1>
+      </div>
+      <div className="App-div4">
+        <h1>3</h1>
+      </div>
     </div>
   );
 }
