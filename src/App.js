4d11c2469710cd9074196732089e5a884d0194b5
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-14 14:27:10 +0530

    background gradient set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 function App() {
+  
+  const ws = new WebSocket('wss://localhost:3000/stg.clubdefy.io/to/ws');
+
+  ws.onmessage = (event) => {
+    console.log(event.data)
+  }
   return (
     <div className="App">
-  
+      
     </div>
   );
 }
