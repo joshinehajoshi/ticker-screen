fd69e5e4e251be4824adfd2fb6302a1d853a97b5
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-15 14:56:26 +0530

    ui completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import image from './images/arrow-right.png';
+import image1 from './images/bitcoin.png';
+import image2 from './images/letter-t.png';
+import image3 from './images/cancel.png';
 
 function App() {
   
@@ -14,27 +18,132 @@ function App() {
         <p className="App-p1">Hi Jhon,</p>
         <p className="App-p2">Complete your KYC</p>
         <p className="App-p3">and experience the world class <br /> bitcoin app defi</p>
-        <p className="App-p4">Start KYC <i class="fa-solid fa-arrow-right-long"></i></p>
+        <p className="App-p4">Start KYC <img src={image} alt="arrow"></img></p>
       </div>
 
 
 
       <div className="App-div2">
-      <p className="App-div2-p1">Hi Jhon,</p>
+      <p className="App-div2-p1">Ideal for new investors</p>
       <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image2} />
+        <p>Ethereum</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image3} />
+        <p>XRP</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
 
       </div>
+
+
+
+      <div className="App-div2 App-div3">
+      <p className="App-div2-p1">Trending coins</p>
+      <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image2} />
+        <p>Ethereum</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image3} />
+        <p>XRP</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
       </div>
 
 
-
-      <div className="App-div3">
-        <h1>2</h1>
+      <div className="App-div2 App-div3">
+      <p className="App-div2-p1">Non trending coins</p>
+      <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
       </div>
+      <br />
 
+      <div className="App-div2-div">
+        <img src={image2} />
+        <p>Ethereum</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
 
-      <div className="App-div4">
-        <h1>3</h1>
+      <div className="App-div2-div">
+        <img src={image3} />
+        <p>XRP</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
+      <div className="App-div2-div">
+        <img src={image1} />
+        <p>Bitcoin</p>
+        <p>1st Quantity</p>
+        <p>2nd Quantity</p>
+        <p>3rd Quantity</p>
+      </div>
+      <br />
+
       </div>
     </div>
   );
